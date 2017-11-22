e1d0293fd85eecd20739412d21785f79d00294cd
Ivan Lim <aimprove@gmail.com>
2017-11-22 13:45:00 -0500

    Add a few changes to test deployment

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        About Us
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         vue-nuxt
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Nuxt.js project by Ivan Lim
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
